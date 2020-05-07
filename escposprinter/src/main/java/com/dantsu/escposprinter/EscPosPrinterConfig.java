bb7a434bb6749eb73d98bc399e783f97af9a767d
Tomas Ivan <dev.tomas.ivan@gmail.com>
2020-05-08 00:08:30 +0200

    config class to change encoding

---
+++ escposprinter/src/main/java/com/dantsu/escposprinter/EscPosPrinterConfig.java
@@ -0,0 +1,6 @@
+package com.dantsu.escposprinter;
+
+public final class EscPosPrinterConfig {
+    public static int CHARACTER_TABLE = 0x06;
+    public static String CHARSET_ENCODING = "ISO-8859-1";
+}
