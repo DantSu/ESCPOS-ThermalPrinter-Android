c69f4bc2c661f2eac85fdb5a875936f0076e7352
Franck ALARY <dantsu@hotmail.com>
2020-05-25 15:02:06 +0200

    fix Bluetooth connection null array exception crash

--- escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothConnection.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/connection/bluetooth/BluetoothConnection.java
@@ -2,6 +2,7 @@ package com.dantsu.escposprinter.connection.bluetooth;
 
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.os.ParcelUuid;
 
 import com.dantsu.escposprinter.connection.DeviceConnection;
 
@@ -40,8 +41,15 @@ public class BluetoothConnection extends DeviceConnection {
         if(this.isConnected()) {
             return true;
         }
+
+        ParcelUuid[] uuid = this.device.getUuids();
+
+        if(uuid == null || uuid.length == 0) {
+            return false;
+        }
+
         try {
-            this.socket = this.device.createRfcommSocketToServiceRecord(this.device.getUuids()[0].getUuid());
+            this.socket = this.device.createRfcommSocketToServiceRecord(uuid[0].getUuid());
             this.socket.connect();
             this.stream = this.socket.getOutputStream();
             this.data = new byte[0];
